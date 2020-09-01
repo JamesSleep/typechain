f532aa71175ecf0fe8ed73b595c31540f82b1096
김동하 <dongha@gimdonghaui-Macmini.local>
2020-09-02 00:37:37 +0900

    #0.4 First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Sleep", age = 24, gender = true;
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender ? "male" : "female"}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("hello");
+const name = "Sleep", age = 24, gender = true;
+
+const sayHi = (name , age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender?"male":"female"}`);
+};
+
+sayHi(name, age);
+
+export {};
