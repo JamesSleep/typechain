b54b95d1e76bc1f19f900750498b1686212066ff
김동하 <dongha@gimdonghaui-Macmini.local>
2020-09-15 23:22:04 +0900

    #0.7 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: boolean;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: boolean;
+  constructor(name: string, age: number, gender: boolean) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
+
+const sleep = new Human("Sleep", 30, true);
 
 const person = {
   name: "Sleep",
@@ -14,6 +21,6 @@ const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender?"male":"female"}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(sleep));
 
 export {};
